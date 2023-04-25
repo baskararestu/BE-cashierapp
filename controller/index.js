439f669f511bda00abe25aa03574e787a46851d1
baskara restu <baskararw10@gmail.com>
2023-04-25 14:12:06 +0700

    fix index.js controller

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,11 @@
 const authController = require('./auth.controller')
 const productController = require('./product.controller')
+const transactionController = require('./transaction.controller')
+const categoryController = require('./category.controller')
 
 module.exports = {
   authController,
   productController,
+  transactionController,
+  categoryController,
 }
