ec2be08bf7c3374e0cc22c5db8995e25fbe1fe0f
Aditya Rizki Pratama <arizki717@gmail.com>
2023-04-25 13:03:17 +0700

    FEAT: (add view product)

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -31,4 +31,41 @@ module.exports = {
       res.status(500).json({ message: 'Internal server error' })
     }
   },
+  getProduct: async (req, res) => {
+    try {
+      const authToken = req.headers.authorization?.split(' ')[1] // extract the token from the headers
+      if (!authToken) {
+        return res.status(401).json({ message: 'Unauthorized' })
+      }
+      console.log(authToken)
+      const userToken = jwt.verify(authToken, 'group08') // verify the token using your secret key
+      // if the token is valid, the decodedToken object will contain the user's ID
+      const userId = userToken.id
+
+      const page = parseInt(req.query.page) || 1
+      const limit = parseInt(req.query.limit) || 10
+      const offset = (page - 1) * limit
+
+      const countSql = `SELECT COUNT(*) as total FROM products WHERE id_user = ?`
+      const [countRows] = await db.query(countSql, [userId])
+      const totalRecords = countRows[0].total
+      const totalPages = Math.ceil(totalRecords / limit)
+
+      const sql = `SELECT p.*, c.name as category_name FROM products p JOIN categories c ON p.id_category = c.id_category WHERE p.id_user = ? LIMIT ? OFFSET ?`
+      const [rows] = await db.query(sql, [userId, limit, offset])
+
+      res.status(200).json({
+        message: 'Get product success',
+        data: rows,
+        pages:{
+          current: page,
+          total: totalPages
+        }
+      })
+
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ message: 'Internal server error' })
+    }
+  }
 }
--- router/product.router.js
+++ router/product.router.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const { productController } = require('../controller')
 
 router.post('/add-product', productController.addProductsCurrentUser)
+router.get('/', productController.getProduct)
 
 module.exports = router
